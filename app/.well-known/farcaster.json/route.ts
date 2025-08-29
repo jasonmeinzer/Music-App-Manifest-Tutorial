64ef245a5f61422d43c9198120f3d78a67f8b990
Jason Meinzer <jasonmeinzer@jasonmeinzer.com>
2025-08-28 19:08:58 -0700

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  return NextResponse.redirect('https://api.farcaster.xyz/miniapps/hosted-manifest/0198f373-46d9-4330-f9b5-d51b4a4b4a50')
-}
+  return NextResponse.redirect('https://api.farcaster.xyz/miniapps/hosted-manifest/0198f373-46d9-4330-f3b5-d51b4a4b4a50')
